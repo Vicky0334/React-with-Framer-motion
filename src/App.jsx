2fcd45af7cec1c8449e214090fa4f24962759b82
Vicky Dhote <dhoteji034@gmail.com>
2024-06-26 18:31:58 +0530

    framer-motion

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <motion.div  id="box"
+      >
+
+      </motion.div>
+    </div>
+  )
+}
+
+export default App
