e8695dd365f44d9717e121c789ec8c03dea267e4
Vicky Dhote <dhoteji034@gmail.com>
2024-06-26 23:05:13 +0530

    Use { animate,variant, initial, transition }

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 
-import { motion } from 'framer-motion'
+import { delay, motion } from 'framer-motion'
 import React from 'react'
 
 const App = () => {
+  const anim={
+    initial:{
+        opacity: 0,
+        scale: 0.2,
+        
+       },
+    final:{
+      x:1000,
+      opacity: 1,
+      scale: 1,
+      rotate:360,
+      borderRadius: "30px",
+      transition: {
+        duration:2,
+        delay:0.7,
+        ease: 'easeOut',
+      },
+    }
+  }
   
   return (
     <div>
-      <motion.div  id="box"
+      <motion.div id="box"
+      variants={anim}
+      initial='initial'
+      animate='final'
       >
-
-      </motion.div>
+     </motion.div>
     </div>
   )
 }
