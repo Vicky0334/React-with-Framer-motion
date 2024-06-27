95d5fbdaa85e29c58ddf8f21962696b0f376aebd
Vicky Dhote <dhoteji034@gmail.com>
2024-06-27 19:25:48 +0530

    Drag & WhileHover

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,42 @@ import { delay, motion } from 'framer-motion'
 import React from 'react'
 
 const App = () => {
-  const anim={
-    initial:{
-        opacity: 0,
-        scale: 0.2,
+  // const anim={
+  //   initial:{
+  //       opacity: 0,
+  //       scale: 0.2,
         
-       },
-    final:{
-      x:1000,
-      opacity: 1,
-      scale: 1,
-      rotate:360,
-      borderRadius: "30px",
-      transition: {
-        duration:2,
-        delay:0.7,
-        ease: 'easeOut',
-      },
-    }
-  }
+  //      },
+  //   final:{
+  //     x:1000,
+  //     opacity: 1,
+  //     scale: 1,
+  //     rotate:360,
+  //     borderRadius: "30px",
+  //     transition: {
+  //       duration:2,
+  //       delay:0.7,
+  //       ease: 'easeOut',
+  //     },
+  //   }
+  // }
   
   return (
     <div>
       <motion.div id="box"
-      variants={anim}
-      initial='initial'
-      animate='final'
+      // variants={anim}
+      // initial='initial'
+      // animate='final'
+       drag
+       whileHover={{
+         scale: 1.2,
+         rotate: 360,
+         transition: {
+           duration:1,
+           ease: 'easeOut',
+         },
+       }}
+       dragConstraints={{ left: 0, right:900, top:0, bottom:50 }}
       >
      </motion.div>
     </div>
